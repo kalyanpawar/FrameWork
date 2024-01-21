b8cb71d92cf76d780aaa7653075f59827d993dda
kalyanpawar <122731787+kalyanpawar@users.noreply.github.com>
2024-01-22 03:58:00 +0530

    Update Baseclass.java
    Headless chrime

--- src/main/java/com/april/Utility/Baseclass.java
+++ src/main/java/com/april/Utility/Baseclass.java
@@ -8,6 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Baseclass {
 	public static WebDriver driver;
@@ -15,8 +16,12 @@ public class Baseclass {
 	public void Setup() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\ChromeDriver\\chromedriver.exe");
 
+ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-gpu");  // Required for headless mode on some systems
+
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(chromeOptions);
 		driver.get("https://testautomationpractice.blogspot.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
